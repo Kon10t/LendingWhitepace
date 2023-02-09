6652794593b73657d88a21a59a8f48c7a2b2a761
Kon10t <101184174+Kon10t@users.noreply.github.com>
2023-02-09 13:13:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let div = createElement('div');
+    div.innerHTML = "<span></span>"
+
+    class Card {
+        constructor(src, title, text) {
+            this.src = src;
+            this.title = title;
+            this.text = text;
+        }
+    
+        render() {
+    
+        }
+    }
+
+});
